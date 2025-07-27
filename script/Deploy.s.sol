ba1e6044670c4fb2806378aa753020d379a1bb0a
Luke-Liu-Alloyx <Luke.Liu@alloyx.com>
2025-07-27 18:53:52 +0800

    style: update deploy.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {console} from "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
@@ -9,11 +10,24 @@ contract CounterScript is Script {
 
     function setUp() public {}
 
-    function run() public {
+    function getChainID() public view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+
+     function run() public {
         vm.startBroadcast();
+        uint256 chainId = getChainID();
+        console.log("chain id: ", chainId);
+        console.log("Deployer: ", msg.sender);
 
         counter = new Counter();
 
         vm.stopBroadcast();
+        console.log("counter deployed at: ", address(counter));
+
     }
 }
